e36a00e9d07fc129c5a6468bb8e81a42bf47a1ad
Rick Harrison <r@rickharrison.me>
2016-07-07 11:20:32 -0700

    Upgrades eslint-plugin-react to 5.2.2

--- index.js
+++ index.js
@@ -265,35 +265,48 @@ module.exports = {
      * React Plugin
      */
 
-    "react/display-name": [2, { "acceptTranspilerName": true }],
+    "react/display-name": [2, { "ignoreTranspilerName": false }],
     "react/forbid-prop-types": [2, { "forbid": ["any"] }],
     "react/jsx-boolean-value": [2, "always"],
     "react/jsx-closing-bracket-location": [2, { "selfClosing": "after-props", "nonEmpty": "after-props"}],
     "react/jsx-curly-spacing": [2, "never"],
+    "react/jsx-equals-spacing": [2, "never"],
+    "react/jsx-filename-extension": [2, { "extensions": [".jsx"] }],
+    "react/jsx-first-prop-new-line": 0,
     "react/jsx-handler-names": 2,
+    "react/jsx-indent": [2, 2],
     "react/jsx-indent-props": [2, 2],
     "react/jsx-key": 2,
     "react/jsx-max-props-per-line": [2, { "maximum": 5 }],
     "react/jsx-no-bind": 2,
     "react/jsx-no-duplicate-props": 2,
     "react/jsx-no-literals": 0,
+    "react/jsx-no-target-blank": 1,
     "react/jsx-no-undef": 2,
     "react/jsx-pascal-case": 2,
     "react/jsx-sort-props": 0,
-    "react/jsx-sort-prop-types": 0,
+    "react/jsx-space-before-closing": 0,
     "react/jsx-uses-react": 2,
     "react/jsx-uses-vars": 2,
+    "react/no-comment-textnodes": 2,
     "react/no-danger": 2,
+    "react/no-deprecated": 2,
     "react/no-did-mount-set-state": [2, "allow-in-func"],
     "react/no-did-update-set-state": [2, "allow-in-func"],
     "react/no-direct-mutation-state": 2,
+    "react/no-is-mounted": 2,
     "react/no-multi-comp": [2, { "ignoreStateless": true }],
+    "react/no-render-return-value": 2,
     "react/no-set-state": 0,
+    "react/no-string-refs": 0,
     "react/no-unknown-property": 2,
     "react/prefer-es6-class": 0,
+    "react/prefer-stateless-function": 1,
     "react/prop-types": 2,
     "react/react-in-jsx-scope": 2,
     "react/require-extension": 0,
+    "react/require-optimization": 0,
+    "react/require-render-return": 2,
     "react/self-closing-comp": 2,
     "react/sort-comp": [2, {
       order: [
@@ -323,6 +336,7 @@ module.exports = {
         ]
       }
     }],
+    "react/sort-prop-types": 0,
     "react/wrap-multilines": 2
   }
 }
